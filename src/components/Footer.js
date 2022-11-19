94807e74ac4a32e928fc113bbf93e94f42030a34
GiovaneBarnes <64332692+GiovaneBarnes@users.noreply.github.com>
2022-11-18 23:08:43 -0500

    Newest changes w/ category

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer(props) { 
+    
+    return(
+        <div className="footer">
+            {props.text}
+        </div>
+    )
+
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header(props) {
+
+    return(
+        <div className="header">
+            {props.title}
+        </div>
+    )
+
+}
+
+export default Header;
